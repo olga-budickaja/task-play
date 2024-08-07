b26638b467f2a9ec48504613e0118525f9b5f63f
Olga Budickaja <olgabudickaja@gmail.com>
2024-08-07 11:30:08 +0300

    added header

--- src/app/modules/content/components/header/header.component.ts
+++ src/app/modules/content/components/header/header.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  buttonText = 'Написати всім за 5';
 
+  users = new Array(9).fill('/users/user-1.png');
+  containerWidth = 250;
+  imageWidth = 40;
+
+  calculateOffset(index: number, total: number): number {
+    const maxOverlap = this.containerWidth - this.imageWidth;
+    const step = maxOverlap / (total - 1);
+    return (-step / 1.7) * index;
+  }
 }
