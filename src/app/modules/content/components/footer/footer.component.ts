8951370bd5e2ef492afac8b079d0ae1c87164dc6
Olga Budickaja <olgabudickaja@gmail.com>
2024-08-08 15:53:10 +0300

    added footer

--- src/app/modules/content/components/footer/footer.component.ts
+++ src/app/modules/content/components/footer/footer.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { ListItem } from './types';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  lists: ListItem[] = [
+    {
+      id: 1,
+      icon: '/buttons/menu/menu-link-1.png',
+      link: '#',
+    },
+    {
+      id: 2,
+      icon: '/buttons/menu/menu-link-2.png',
+      link: 'cards',
+    },
+    {
+      id: 3,
+      icon: '/buttons/menu/menu-link-3.png',
+      link: '#',
+    },
+    {
+      id: 4,
+      icon: '/buttons/menu/menu-link-4.png',
+      link: '#',
+    },
+    {
+      id: 5,
+      icon: '/buttons/menu/menu-link-5.png',
+      link: '#',
+    },
+  ];
 
+  activeIndex: number | null = null;
+
+  setActive(index: number) {
+    this.activeIndex = index;
+  }
 }
---
+++ src/app/modules/content/components/footer/types.ts
@@ -0,0 +1,5 @@
+export type ListItem = {
+  id: number;
+  icon: string;
+  link: string;
+};
